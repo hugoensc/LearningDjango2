10550e0186e328629b1f7dae34a37bc658937dfc
Hugo Santamaria <hugo.santamaria@alianzacorp.mx>
2019-10-25 10:50:06 -0500

    Agregar archivos de proyecto.

---
+++ envLearningDjango2/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\hugo.santamaria\source\repos\LearningDjango2\envLearningDjango2\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
